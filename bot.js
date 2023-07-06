54bcc3d09d4a435963ad10cee140cdc33fe4638a
Nisay <Ydehfuli@gmail.com>
2023-07-06 21:53:59 +0330

    Config bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+const TelegramBot = require('node-telegram-bot-api');
+// replace the value below with the Telegram token you receive from @BotFather
+const token = 'TOKEN-HERE';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, msg => {
+    bot.sendMessage(msg.chat.id,'salaM '+ msg.chat.first_name + ' bE boT maN khoshoomadI :)'
+
+    )
+})
+
