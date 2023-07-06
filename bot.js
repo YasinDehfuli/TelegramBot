d37b1042dd3f71d0ba857d02f57372d82a4a9a55
Nisay <Ydehfuli@gmail.com>
2023-07-06 21:56:51 +0330

    Add some text bot.js answers

--- bot.js
+++ bot.js
@@ -20,3 +20,22 @@ bot.onText(/\/start/, msg => {
 
 })
 
+bot.on('message', msg=>{
+    console.log(msg.text)
+        if (msg.text === 'سلام'){
+            bot.sendVideo(msg.chat.id , 'BAACAgQAAxkBAAMqZKWx8mUHfGqd_palKdy_EqJcL_kAApsQAAIkYDBRBRN-XOrO-Q0vBA')
+            bot.sendMessage(msg.chat.id, 'سلامممممممممممممم !')
+        }
+        if (msg.text === 'خوبی'){
+            bot.sendMessage(msg.chat.id, 'من به عنوان یه ربات نمیتوانم احساس داشته باشم! یوهوهو :)')
+        }
+        if (msg.text === 'کیو دوست داری' || msg.text === 'عاشق کی ایی'){
+            bot.sendMessage(msg.chat.id, 'تورو :) 🖤')
+        }
+        if (msg.text === 'چند تا' || msg.text === 'چند تا دوسم داری'){
+            bot.sendMessage(msg.chat.id, 'infinite loop started...')
+            for (let i = 0; i < 10; i++) {
+                bot.sendMessage(msg.chat.id,'🖤')
+            }
+        }
+})
